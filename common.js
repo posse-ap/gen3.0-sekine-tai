e7e46019d310315e874ab82c094f12fc88ddcb19
Taisei Sekine <taisei.s.sub2@gmail.com>
2022-09-03 00:23:04 +0900

    week9-in_progress

---
+++ common.js
@@ -0,0 +1,11 @@
+'use strict';
+
+{
+  const Header = document.getElementById('js-header');
+  const HeaderButton = document.getElementById('js-headerButton');
+
+  HeaderButton.addEventListener('click', () => {
+    Header.classList.toggle('is-open')
+    HeaderButton.classList.toggle('clicked')
+  })
+}

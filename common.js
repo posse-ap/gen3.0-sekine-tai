66beea7170b2e80440f10e9367461aa7e992ef19
Taisei Sekine <taisei.s.sub2@gmail.com>
2022-09-12 00:52:16 +0900

    week9-finished

--- common.js
+++ common.js
@@ -2,10 +2,14 @@
 
 {
   const Header = document.getElementById('js-header');
+  const menu = document.getElementById ('js-menu');
   const HeaderButton = document.getElementById('js-headerButton');
+  const bannar = document.getElementById('bannar');
 
   HeaderButton.addEventListener('click', () => {
     Header.classList.toggle('is-open')
     HeaderButton.classList.toggle('clicked')
+    menu.classList.toggle('is-picked')
+    bannar.classList.toggle('opened')
   })
 }
